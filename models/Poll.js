b3550673e9bd062b9803385c412be890bf33ee12
minthug <jkisimmortal4@gmail.com>
2025-10-24 15:48:47 +0900

    Create Poll.js

---
+++ models/Poll.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    votes: {
+        type: Number,
+        default: 0
+    }
+});
