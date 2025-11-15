d1591c9c05effb19279040256ddd04de75cb7d42
minthug <jkisimmortal4@gmail.com>
2025-11-15 18:38:13 +0900

    Update route/polls.js, models/Poll.js

--- routes/polls.js
+++ routes/polls.js
@@ -92,6 +92,7 @@ router.post('/:id/vote', async (req, res) => {
 
         // 투표 증가
         option.votes += 1;
+        await poll.save();
 
         // 투표한 IP 추가 (Poll 모델에 voteIps 필드가 추가 되어야 함)
         if (!poll.voteIps) poll.voteIps = [];
