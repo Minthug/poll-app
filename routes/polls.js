1e61cf497ab147eb95b9afdeb060b6cebb3ce75f
minthug <jkisimmortal4@gmail.com>
2025-12-17 15:23:21 +0900

    Add partials/ranking-widget.ejs file

--- routes/polls.js
+++ routes/polls.js
@@ -92,7 +92,7 @@
         try {
             const poll = await Poll.findById(req.params.id);
             
-            await Poll.findByIdAndUpdate(req.params.id, { $inc: { view: 1 }});
+            await Poll.findByIdAndUpdate(req.params.id, { $inc: { views: 1 }});
 
             if (!poll) {
                 return res.status(404).send('여론조사를 찾을 수 없습니다');
