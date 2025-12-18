f16cabea5af4e680c814e08ff06c0b5c7ccff000
minthug <jkisimmortal4@gmail.com>
2025-12-18 15:28:31 +0900

    Task 1: Only logged-in users can create opinion polls

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,4 +6,14 @@ function checkAuth(req, res, next) {
     res.redirect('/admin/login');
 }
 
+// 일반 사용자 로그인 체크 (NEW)
+function requireLogin(req, res, next) {
+    if (req.session.userId) {
+        next();
+    } else {
+        // 로그인 페이지로 리다이렉션하면서 원래 가려던 URL 저장
+        req.session.returnTo = req.originalUrl;
+    }
+}
+
 module.exports = { checkAuth };
