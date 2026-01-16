45f7abe2a2aa3225d2c8407bdcfb100a5b29d9a0
minthug <jkisimmortal4@gmail.com>
2026-01-16 17:06:36 +0900

    Fix sent verified email Main Content is change port  Step 3

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const { body, validationResult } = require('express-validator');
 // ========================================
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
