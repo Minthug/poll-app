42b843e5f536030bdc99407d7acec0c89b6c3141
minthug <jkisimmortal4@gmail.com>
2026-01-19 16:05:54 +0900

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -30,6 +30,34 @@ const commentSchema = new mongoose.Schema({
 
     // 댓글 내용
     content: {
-        
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 1000
+    },
+
+    // 상태
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+    deleteBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    // IP 정보 (신고/차단용)
+    ipAddress: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
     }
 });
+
+commentSchema.index({ pollId: 1, createdAt: -1 });
+module.exports = mongoose.model('Comment', commentSchema);
