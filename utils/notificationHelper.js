52be777b2c605f09692f09667d67a017a0c6f6cd
minthug <jkisimmortal4@gmail.com>
2026-01-22 17:30:58 +0900

    Implementation of notification functionality and user settings, among other code features Step 4

---
+++ utils/notificationHelper.js
@@ -0,0 +1,70 @@
+const Notification = require('../models/Notification');
+const User = require('../models/User');
+const user = require('../models/User');
+const nodemailer = require('nodemailer');
+
+class NotificationHelper {
+    static async createNotification({
+        recipientId,
+        senderId,
+        type,
+        pollId,
+        commentId,
+        message,
+        link
+    }) {
+        try {
+            // 수신자의 알림 설정 확인
+            const recipient = await User.findById(recipientId);
+            if (!recipient || !recipient.notificationSettings.enabled) {
+                return null;
+            }
+
+            const settingMap = {
+                'comment': 'comments',
+                'reply': 'replies',
+                'vote': 'votes',
+                'poll_end': 'pollEnd'
+            };
+
+            const settingKey = settingMap[type];
+            if (settingKey && !recipient.notificationSettings[settingKey]) {
+                return null;
+            }
+
+            // 자기 자신에게 알림 보내지않기
+            if (senderId && recipientId.toString() === senderId.toString()) {
+                return null;
+            }
+
+            const notification = await Notification.create({
+                recipient: recipientId,
+                sender: senderId,
+                type,
+                poll: pollId,
+                comment: commentId,
+                message,
+                link
+            });
+
+            if (recipient.notificationSettings.email.enabled) {
+                const emailSettingMap = {
+                    'comment': 'comments',
+                    'reply': 'replies'
+                };
+
+                const emailSettingKey = emailSettingMap[type];
+                if (emailSettingKey && recipient.notificationSettings.email[emailSettingKey]) {
+                    await this.sendEmailNotification(recipient.email, notification);
+                }
+            }
+
+            return notification;
+        } catch (error) {
+            console.error('알림 생성 오류:', error);
+            return null;
+        }
+    }
+
+    
+}
